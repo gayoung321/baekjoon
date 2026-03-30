763ca16b0678d03ca8176f6e9478123f4ba3b4e8
gayoung321 <lgy4708@gmail.com>
2026-03-31 00:28:00 +0900

    Create 6-9498.py

---
+++ 6-9498.py
@@ -0,0 +1,11 @@
+n=int(input())
+if (n>=90):
+    print('A')
+elif (n<=89 and n>=80 ):
+    print('B')
+elif (n<=79 and n>=70 ):
+    print('C')
+elif (n<=69 and n>=60 ):
+    print('D')
+else:
+    print('F')
